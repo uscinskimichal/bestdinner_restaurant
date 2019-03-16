7732398c916f405ffc8481d7940aab7cd88adf0e
agorski <adriangorski996@gmail.com>
2019-03-16 20:11:51 +0100

    projekt test controller fix

---
+++ src/main/java/pl/bestdinner/controller/TestController.java
@@ -0,0 +1,18 @@
+package pl.bestdinner.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tst")
+public class TestController {
+
+  @GetMapping
+  public ResponseEntity<String> tst(){
+    return new ResponseEntity<>("Wygląda na to, że wszystko działa", HttpStatus.OK);
+  }
+
+}
