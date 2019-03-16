103a443245b5da26b532dc2e95d81da6239d84c7
agorski <adriangorski996@gmail.com>
2019-03-16 19:49:35 +0100

    project init

---
+++ src/main/java/pl/bestdinner/BestDinnerApplication.java
@@ -0,0 +1,14 @@
+package pl.bestdinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestDinnerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BestDinnerApplication.class, args);
+  }
+}
+
+
