60b4ce0333a1fa0406dff6f47cdecbd96176afe7
AdKoch <adam.kochanski.97@gmail.com>
2019-06-10 18:53:08 +0200

    OrderItem Controller pt2

---
+++ src/main/java/pl/bestdinner/controller/OrderItemController.java
@@ -0,0 +1,47 @@
+package pl.bestdinner.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.bestdinner.dto.OrderItemDto;
+import pl.bestdinner.service.OrderItemService;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/order_items")
+public class OrderItemController {
+
+    private final OrderItemService service;
+
+    public OrderItemController(OrderItemService service) {
+        this.service = service;
+    }
+
+    @GetMapping(produces = "application/json;charset=UTF-8") // można dorzucić  produces, żeby jawnie ustawić kodowanie
+    public ResponseEntity<List<OrderItemDto>> getAll() {
+        return new ResponseEntity<>( service.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
+    public ResponseEntity<OrderItemDto> create(@RequestBody OrderItemDto requestBody) {
+        return new ResponseEntity<>(service.create(requestBody), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity delete(@PathVariable("id") Integer id){
+        service.delete(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OrderItemDto> get(@PathVariable("id") Integer id){
+        return new ResponseEntity<>(service.get(id), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<OrderItemDto> update(@RequestBody OrderItemDto requestBody,
+                                                @PathVariable("id") Integer id){
+        return new ResponseEntity<>(service.update(requestBody, id), HttpStatus.OK);
+    }
+}
