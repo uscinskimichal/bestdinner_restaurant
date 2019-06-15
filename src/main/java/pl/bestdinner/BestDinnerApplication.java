2fe451c8d54838836da5e538b64b0d99ed03b84b
agorski <adriangorski996@gmail.com>
2019-06-15 02:45:42 +0200

    dodanie swaggera

--- src/main/java/pl/bestdinner/BestDinnerApplication.java
+++ src/main/java/pl/bestdinner/BestDinnerApplication.java
@@ -2,6 +2,8 @@ package pl.bestdinner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -10,6 +12,9 @@ public class BestDinnerApplication {
   public static void main(String[] args) {
     SpringApplication.run(BestDinnerApplication.class, args);
   }
+
+
+
 
 }
 
