c56d70456a8060db27cc424279db071ad6a03da2
Michał <uscinskimichal@gmail.com>
2019-06-19 19:19:25 +0200

    Changed DishType JsonProperty

--- src/main/java/pl/bestdinner/dto/DishTypeDto.java
+++ src/main/java/pl/bestdinner/dto/DishTypeDto.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 public class DishTypeDto {
 
-    @JsonProperty("dishType_id")
+    @JsonProperty("dish_type_id")
     private Long dishTypeId;
 
     @JsonProperty("name")
